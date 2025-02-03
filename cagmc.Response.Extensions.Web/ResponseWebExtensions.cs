5530b6ea98537bf3d4f5438f14ce34c1a4ec3445
istvan.rozsa <istvan.rozsa@outlook.com>
2025-02-03 22:46:24 +0100

    Add Web extension project for converting responses to IActionResult
    Introduced a new project, `cagmc.Response.Extensions.Web`, for ASP.NET Core integrations. This includes an extension method `ToActionResult` for converting `Response` objects into `IActionResult`. Updated the solution file to include the new project.

---
+++ cagmc.Response.Extensions.Web/ResponseWebExtensions.cs
@@ -0,0 +1,11 @@
+﻿using cagmc.Response.Core;
+
+namespace Microsoft.AspNetCore.Mvc;
+
+public static class ResponseWebExtensions
+{
+    public static IActionResult ToActionResult(this Response response)
+    {
+        return new ObjectResult(response) { StatusCode = response.Code };
+    }
+}
