d4a550d2f83c2d0bef94a5ab3a3337a8aa0a30ef
istvan.rozsa <istvan.rozsa@outlook.com>
2025-11-29 14:08:19 +0100

    Remove `IsSuccess` initialization from error responses
    Streamline error response creation by omitting redundant `IsSuccess` initialization.

--- cagmc.Response.Core/ResponseBase`1.cs
+++ cagmc.Response.Core/ResponseBase`1.cs
@@ -5,7 +5,8 @@
 /// providing common properties and methods to standardize the handling of operation outcomes.
 /// </summary>
 /// <typeparam name="TResponse">The specific type of the response derived from this base class.</typeparam>
-public abstract record ResponseBase<TResponse> where TResponse : ResponseBase<TResponse>, new()
+public abstract record ResponseBase<TResponse> 
+    where TResponse : ResponseBase<TResponse>, new()
 {
     public bool IsSuccess { get; init; }
     public int Code { get; init; }
@@ -16,15 +17,15 @@ public abstract record ResponseBase<TResponse> where TResponse : ResponseBase<TR
     public bool IsServerError => Code is >= 500 and < 600;
     
     public static TResponse Success => new() { IsSuccess = true, Code = 200 };
-    public static TResponse BadRequest => new() { IsSuccess = false, Code = 400 };
-    public static TResponse Unauthorized => new() { IsSuccess = false, Code = 401 };
-    public static TResponse Forbidden => new() { IsSuccess = false, Code = 403 };
-    public static TResponse NotFound => new() { IsSuccess = false, Code = 404 };
-    public static TResponse Conflict => new() { IsSuccess = false, Code = 409 };
-    public static TResponse InternalServerError => new() { IsSuccess = false, Code = 500 };
+    public static TResponse BadRequest => new() { Code = 400 };
+    public static TResponse Unauthorized => new() { Code = 401 };
+    public static TResponse Forbidden => new() { Code = 403 };
+    public static TResponse NotFound => new() { Code = 404 };
+    public static TResponse Conflict => new() { Code = 409 };
+    public static TResponse InternalServerError => new() { Code = 500 };
     
-    public static TResponse CreateError(int code, string? message = null) => new() { IsSuccess = false, Code = code, Message = message };
-    public static TResponse CreateBadRequest(string? message = null) => new() { IsSuccess = false, Code = 400, Message = message };
+    public static TResponse CreateError(int code, string? message = null) => new() { Code = code, Message = message };
+    public static TResponse CreateBadRequest(string? message = null) => new() { Code = 400, Message = message };
     
-    public static TResponse FromException(Exception ex) => new() { IsSuccess = false, Code = 500, Message = ex.Message };
+    public static TResponse FromException(Exception ex) => new() { Code = 500, Message = ex.Message };
 } 
